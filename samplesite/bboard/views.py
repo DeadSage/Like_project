546611ad764aad41f49b11d28bebe3fe0ec92d13
DeadSage <olegnaruto1012001@gmail.com>
2021-04-18 16:35:52 +0300

    first commit

---
+++ samplesite/bboard/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Bb, Rubric
+from django.template import loader
+from .forms import BbForm
+from django.views.generic.edit import CreateView
+
+
+def index(request):
+    bbs = Bb.objects.all()
+    rubrics = Rubric.objects.all()
+    context = {'bbc': bbs, 'rubrics': rubrics}
+    return render(request, 'bboard/index.html', context)
+
+
+def by_rubric(request, rubric_id):
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {'bbc': bbs, 'rubrics': rubrics, 'current_rubric': current_rubric}
+    return render(request, 'bboard/by_rubric.html', context)
+
+
+class BbCreateView(CreateView):
+    template_name = 'bboard/create.html'
+    form_class = BbForm
+    success_url = '/bboard/'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
