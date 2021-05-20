c54b3f8f3bd9655a2dcba4d47b3511a74aaf1a6a
DeadSage <79532594+DeadSage@users.noreply.github.com>
2021-05-20 17:59:31 +0300

    bboard classView

--- samplesite/bboard/urls.py
+++ samplesite/bboard/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import index, by_rubric, BbCreateView
+from .views import index, by_rubric
+from .views import add_and_save, BbDetailView
+# from .views import download
 
 urlpatterns = [
     path('<int:rubric_id>/', by_rubric, name='by_rubric'),
-    path('add/', BbCreateView.as_view(), name='add'),
+    path('add/', add_and_save, name='add'),
     path('', index, name='index'),
+    path('detail/<int:pk>/', BbDetailView.as_view(), name='detail')
+    # path('download/', download, name='download')
 ]
--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,17 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound, FileResponse
+from django.views.generic.edit import FormView
+
 from .models import Bb, Rubric
-from django.template import loader
+from django.template.loader import get_template
 from .forms import BbForm
 from django.views.generic.edit import CreateView
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+
+
+# def download(request):
+#     filename = '/Users/deadsage14235icloud.com/Desktop/кАзино кАзино/-EOUv6Nmw3E.jpg'
+#     return FileResponse(open(filename, 'rb'), as_attachment=True)
 
 
 def index(request):
     bbs = Bb.objects.all()
     rubrics = Rubric.objects.all()
     context = {'bbs': bbs, 'rubrics': rubrics}
-    return render(request, 'bboard/index.html', context)
+    template = get_template('bboard/index.html')
+    # return render(request, 'bboard/index.html', context)
+    return HttpResponse(template.render(context=context, request=request))
 
 
 def by_rubric(request, rubric_id):
@@ -22,12 +33,62 @@ def by_rubric(request, rubric_id):
     return render(request, 'bboard/by_rubric.html', context)
 
 
-class BbCreateView(CreateView):
-    template_name = 'bboard/create.html'
-    form_class = BbForm
-    success_url = reverse_lazy('index')
+def add_and_save(request):
+    if request.method == 'POST':
+        bbf = BbForm(request.POST)
+        if bbf.is_valid():
+            bbf.save()
+            return HttpResponseRedirect(reverse('by_rubric',
+                                                kwargs={'rubric_id': bbf.cleaned_data['rubric'].pk}))
+        else:
+            context = {'form': bbf}
+            return render(request, 'bboard/create.html', context)
+    else:
+        bbf = BbForm()
+        context = {'form': bbf}
+        return render(request, 'bboard/create.html', context)
+
+
+class BbDetailView(DetailView):
+    model = Bb
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['rubrics'] = Rubric.objects.all()
         return context
+
+
+class BbByRubricView(ListView):
+    template_name = 'bboard/by_rubric.html'
+    context_object_name = 'bbs'
+
+    def get_queryset(self):
+        return Bb.objects.filter(rubric=self.kwargs['rubric_id'])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        context['current_rubric'] = Rubric.objects.get(self.kwargs['rubric_id'])
+        return context
+
+
+class BbAddView(FormView):
+    template_name = 'bboard/create.html'
+    form_class = BbForm
+    initial = {'price': 0.0}
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
+    def get_form(self, form_class=None):
+        self.object = super().get_form(form_class)
+        return self.object
+
+    def get_success_url(self):
+        return reverse('bboard:by_rubric', kwargs={'rubric_id': self.object.cleaned_data['rubric'].pk})
