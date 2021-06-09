57d8fec3d98e7edc781f47879304e30dda42a66e
DeadSage <79532594+DeadSage@users.noreply.github.com>
2021-06-09 12:17:29 +0300

    invalid characters HTML

--- samplesite/bboard/templatetags/filtersandtags.py
+++ samplesite/bboard/templatetags/filtersandtags.py
@@ -1,12 +1,13 @@
 from django import template
 from django.template.defaultfilters import stringfilter
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
 
-@register.filter
+@register.filter(is_save=True)
 def currency(value, name='руб.'):
-    return '%1.2f %s' % (value, name)
+    return mark_safe('%1.2f %s' % (value, name))
 
 
 # register.filter('currency', currency)
