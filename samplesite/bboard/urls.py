9ad7068181bc753100716e8be2b374a60b8b17f1
DeadSage <79532594+DeadSage@users.noreply.github.com>
2021-06-13 12:58:06 +0300

    password reset

--- samplesite/bboard/urls.py
+++ samplesite/bboard/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .views import index, by_rubric
 from .views import add_and_save, BbDetailView, BbEditView, BbDeleteView, RegistrationView
 from django.contrib.auth.views import PasswordChangeView
+from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, \
+    PasswordResetCompleteView
 
 # from .views import download
 
@@ -14,7 +16,13 @@ urlpatterns = [
     path('delete/<int:pk>/', BbDeleteView.as_view(), name='delete'),
     path('registration/', RegistrationView.as_view(), name='registration'),
     # path('download/', download, name='download')
-    path('accounts/password_change/', PasswordChangeView.as_view(
-            template_name='registration/password_change_form.html'
-        ), name='password_change'),
+    # path('accounts/password_change/', PasswordChangeView.as_view(
+    #         template_name='registration/password_change_form.html'
+    #     ), name='password_change'),
+    path('password_reset/', PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password_reset/confirm/',
+         PasswordResetConfirmView, name='password_reset_confirm'),
+    path('password_reset/complete/', PasswordResetCompleteView,
+         name='password_reset_complete'),
 ]
--- samplesite/samplesite/urls.py
+++ samplesite/samplesite/urls.py
@@ -23,26 +23,26 @@ from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView,
 urlpatterns = [
     path('social/', include('social_django.urls', namespace='social')),
     path('', LoginView.as_view(), name='login'),
-    # path('accounts/password_change/', PasswordChangeView.as_view(
-    #     template_name='registration/password_change_form.html'
-    # ), name='password_change'),
+    # # path('accounts/password_change/', PasswordChangeView.as_view(
+    # #     template_name='registration/password_change_form.html'
+    # # ), name='password_change'),
     path('accounts/logout/', LogoutView.as_view(next_page='login'), name='logout'),
-    path('accounts/password_change/done', PasswordChangeDoneView.as_view(
-        template_name='registration/password_change.html'), name='password_change_done'),
-    path('accounts/password_reset/', PasswordResetView.as_view(
-        template_name='registration/reset_password.html',
-        subject_template_name='registration/reset_subject.txt',
-        email_template_name='registration/reset_email.txt'), name='password_reset'),
-    path('accounts/password_reset/done/', PasswordResetDoneView.as_view(
-        template_name='registration/email_sent.html'), name='password_reset_done'),
-    path('accounts/reset/<uidb64>/<token>/',
-         PasswordResetConfirmView.as_view(
-             template_name='registration/confirm_password.html'),
-         name='password_reset_confirm'),
-    path('accounts/reset/done/',
-         PasswordResetCompleteView.as_view(
-             template_name='registration/password_confirmed.html'),
-         name='password_reset_complete'),
+    # path('accounts/password_change/done', PasswordChangeDoneView.as_view(
+    #     template_name='registration/password_change.html'), name='password_change_done'),
+    # path('accounts/password_reset/', PasswordResetView.as_view(
+    #     template_name='registration/reset_password.html',
+    #     subject_template_name='registration/reset_subject.txt',
+    #     email_template_name='registration/reset_email.txt'), name='password_reset'),
+    # path('accounts/password_reset/done/', PasswordResetDoneView.as_view(
+    #     template_name='registration/email_sent.html'), name='password_reset_done'),
+    # path('accounts/reset/<uidb64>/<token>/',
+    #      PasswordResetConfirmView.as_view(
+    #          template_name='registration/confirm_password.html'),
+    #      name='password_reset_confirm'),
+    # path('accounts/reset/done/',
+    #      PasswordResetCompleteView.as_view(
+    #          template_name='registration/password_confirmed.html'),
+    #      name='password_reset_complete'),
     # path('', include('bboard.urls')),
     path('captcha/', include('captcha.urls')),
     path('bboard/', include('bboard.urls')),
