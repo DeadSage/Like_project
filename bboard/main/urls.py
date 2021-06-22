861f1720329d9d01784bbc668af4863de5aaae9d
DeadSage <79532594+DeadSage@users.noreply.github.com>
2021-06-22 18:33:06 +0300

    working on logout, user profile and change user info pages

--- bboard/main/urls.py
+++ bboard/main/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from .views import index, other_page, BBLoginView
+from .views import index, other_page, BBLoginView, profile, BBLogoutView, ChangeUserInfoView
 
 app_name = 'main'
 
 urlpatterns = [
+    path('accounts/profile/change/', ChangeUserInfoView.as_view(), name='profile_change'),
+    path('accounts/logout/', BBLogoutView.as_view(), name='logout'),
+    path('accounts/profile/', profile, name='profile'),
     path('<str:page>/', other_page, name='other'),
     path('', index, name='index'),
     path('accounts/login/', BBLoginView.as_view(), name='login'),
+
 ]
