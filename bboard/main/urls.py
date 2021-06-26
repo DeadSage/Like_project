d5bbc3eb7c832a676cce4c2ba1cddf796704e391
DeadSage <79532594+DeadSage@users.noreply.github.com>
2021-06-26 16:30:06 +0300

    password reset

--- bboard/main/urls.py
+++ bboard/main/urls.py
@@ -1,10 +1,27 @@
 from django.urls import path
+from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, \
+    PasswordResetCompleteView
 from .views import index, other_page, BBLoginView, profile, BBLogoutView, ChangeUserInfoView, BBPasswordChangeView, \
     RegisterUserView, RegisterDoneView, user_activate, DeleteUserView
 
 app_name = 'main'
 
 urlpatterns = [
+    path('accounts/reset/<uidb64>/<token>/',
+         PasswordResetConfirmView.as_view(
+             template_name='registration/confirm_password.html'),
+         name='password_reset_confirm'),
+    path('accounts/reset/done/',
+         PasswordResetCompleteView.as_view(
+             template_name='registration/password_confirmed.html'),
+         name='password_reset_complete'),
+    path('accounts/password_reset/done/', PasswordResetDoneView.as_view(
+        template_name='registration/email_sent.html'), name='password_reset_done'),
+    path('accounts/password_reset/', PasswordResetView.as_view(
+        template_name='registration/reset_password.html',
+        subject_template_name='registration/reset_subject.txt',
+        email_template_name='registration/reset_email.txt',),
+        name='password_reset'),
     path('accounts/profile/delete/', DeleteUserView.as_view(), name='profile_delete'),
     path('accounts/register/activate/<str:sign>', user_activate, name='register_activate'),
     path('accounts/register/done/', RegisterDoneView.as_view(), name='register_done'),
