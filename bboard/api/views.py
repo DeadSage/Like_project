bca44f3ff1b545b4ac9ce634d4cf9d5b520cea6d
DeadSage <79532594+DeadSage@users.noreply.github.com>
2021-07-08 23:35:08 +0300

    ads comments api

--- bboard/api/views.py
+++ bboard/api/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from rest_framework.generics import RetrieveAPIView
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
-
-from main.models import Bb
-from .serializers import BbSerializer, BbDetailSerializer
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from main.models import Bb, Comment
+from .serializers import BbSerializer, BbDetailSerializer, CommentSirializer
 
 
 @api_view(['GET'])
@@ -18,3 +19,19 @@ def bbs(request):
 class BbDetailView(RetrieveAPIView):
     queryset = Bb.objects.filter(is_active=True)
     serializer_class = BbDetailSerializer
+
+
+@api_view(['GET', 'POST'])
+@permission_classes((IsAuthenticatedOrReadOnly,))
+def comments(request, pk):
+    if request.method == 'POST':
+        serializer = CommentSirializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+    else:
+        comments = Comment.objects.filter(is_active=True, bb=pk)
+        serializer = CommentSirializer(comments, many=True)
+        return Response(serializer.data)
