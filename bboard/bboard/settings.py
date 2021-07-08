771c32c090ff6bffa9729b8f0972d0e6390ad4dd
DeadSage <79532594+DeadSage@users.noreply.github.com>
2021-07-08 23:18:11 +0300

    ads api

--- bboard/bboard/settings.py
+++ bboard/bboard/settings.py
@@ -42,9 +42,13 @@ INSTALLED_APPS = [
     'easy_thumbnails',
     'django_cleanup',
     'captcha',
+    'rest_framework',
+    'corsheaders',
+    'api.apps.ApiConfig'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -53,6 +57,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_URLS_REGEX = r'^/api/.*$'
 
 ROOT_URLCONF = 'bboard.urls'
 
