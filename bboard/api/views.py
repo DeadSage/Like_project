e396f83646957133615df2b10adfb4761c58f55f
DeadSage <79532594+DeadSage@users.noreply.github.com>
2021-07-28 18:17:46 +0300

    final commits

--- bboard/api/views.py
+++ bboard/api/views.py
@@ -35,3 +35,5 @@ def comments(request, pk):
         comments = Comment.objects.filter(is_active=True, bb=pk)
         serializer = CommentSirializer(comments, many=True)
         return Response(serializer.data)
+
+
